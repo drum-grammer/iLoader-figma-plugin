6f21854f7beec342515421a9a5ae9c177d539868
drum-grammer <drumgrammer@gmail.com>
2023-09-25 21:46:58 +0900

    구현

--- code.ts
+++ code.ts
@@ -1,66 +1,71 @@
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+const htmlContent = `
+<!DOCTYPE html>
+<html lang="en">
+<body>
+  <input type="text" id="documentKey" placeholder="Document Key" />
+  <button onclick="sendDocumentKey()">Submit</button>
 
-// Runs this code if the plugin is run in Figma
-if (figma.editorType === "figma") {
-  // This plugin creates 5 rectangles on the screen.
-  const numberOfRectangles = 5;
+  <script>
+    function sendDocumentKey() {
+      const documentKey = document.getElementById('documentKey').value;
+      window.parent.postMessage({ pluginMessage: { type: 'document-key', documentKey } }, '*');
+    }
+  </script>
+</body>
+</html>
+`;
 
-  const nodes: SceneNode[] = [];
-  for (let i = 0; i < numberOfRectangles; i++) {
-    const rect = figma.createRectangle();
-    rect.x = i * 150;
-    rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-    figma.currentPage.appendChild(rect);
-    nodes.push(rect);
+type AnswerData = {
+  question_id: string;
+  question_content: string;
+  answers: string[];
+};
+
+type QuestionnaireData = {
+  questionnaire_id: string;
+  questionnaire_title: string;
+  extracts: AnswerData[];
+};
+
+figma.showUI(htmlContent, { width: 240, height: 100 }); // UI를 보여주고 사용자로부터 Document Key를 입력 받습니다.
+
+figma.ui.onmessage = (msg) => {
+  if (msg.type === 'document-key') {
+    const documentKey = msg.documentKey;
+    const url = `https://api-i-loader.52g.studio/data-extracts/key/${documentKey}`; // Document Key를 사용하여 URL을 생성합니다.
+
+
+    fetch(url)
+        .then(response => response.json())
+        .then((data: QuestionnaireData) => createTextNodesFromData(data))
+        .catch(error => console.error('Error:', error));
   }
-  figma.currentPage.selection = nodes;
-  figma.viewport.scrollAndZoomIntoView(nodes);
+};
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
+async function createTextNodesFromData(data: QuestionnaireData) {
+  await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+
+  let yOffset = 0;
+
+  for (const extract of data.extracts) {
+    const questionNode = figma.createText();
+    questionNode.characters = extract.question_content;
+    questionNode.y = yOffset;
+    figma.currentPage.appendChild(questionNode);
+
+    yOffset += questionNode.height + 10; // 10은 간격입니다.
+
+    for (const answer of extract.answers) {
+      const answerNode = figma.createText();
+      answerNode.characters = answer;
+      answerNode.y = yOffset;
+      figma.currentPage.appendChild(answerNode);
+
+      yOffset += answerNode.height + 5; // 5는 간격입니다.
+    }
+
+    yOffset += 20; // 각 질문 사이의 추가 간격입니다.
+  }
+
   figma.closePlugin();
-}
-// Runs this code if the plugin is run in FigJam
-if (figma.editorType === "figjam") {
-  // This plugin creates 5 shapes and 5 connectors on the screen.
-  const numberOfShapes = 5;
-
-  const nodes: SceneNode[] = [];
-//   for (let i = 0; i < numberOfShapes; i++) {
-//     const shape = figma.createShapeWithText();
-//     // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
-//     shape.shapeType = "ROUNDED_RECTANGLE";
-//     shape.x = i * (shape.width + 200);
-//     shape.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-//     figma.currentPage.appendChild(shape);
-//     nodes.push(shape);
-//   }
-
-//   for (let i = 0; i < numberOfShapes - 1; i++) {
-//     const connector = figma.createConnector();
-//     connector.strokeWeight = 8;
-
-//     connector.connectorStart = {
-//       endpointNodeId: nodes[i].id,
-//       magnet: "AUTO",
-//     };
-
-//     connector.connectorEnd = {
-//       endpointNodeId: nodes[i + 1].id,
-//       magnet: "AUTO",
-//     };
-//   }
-
-figma.loadFontAsync({ family: "Inter", style: "Medium" }).then(() => {
-    const textNode = figma.createText();
-    textNode.characters = "Hello world!";
-    figma.currentPage.appendChild(textNode);
-
-    figma.currentPage.selection = [textNode];
-    figma.viewport.scrollAndZoomIntoView([textNode]);
-    figma.closePlugin();
-  });
 }
