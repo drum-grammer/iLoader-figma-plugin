77d5cc95caa8eaec84a46753d90f3cf0f30d1a9f
drum-grammer <drumgrammer@gmail.com>
2023-09-26 11:37:47 +0900

    ADD. 질문에 대한 답변을 감싸는 박스에 패딩을 추가, 답변 생상을 검정으로 고정

--- code.ts
+++ code.ts
@@ -43,11 +43,13 @@ figma.ui.onmessage = (msg) => {
 };
 
 async function createTextNodesFromData(data: QuestionnaireData) {
+  // 필요한 폰트를 로드합니다.
   await figma.loadFontAsync({ family: "Inter", style: "Regular" });
   await figma.loadFontAsync({ family: "Inter", style: "Medium" });
 
   let yOffset = 0;
 
+  // 각 질문과 답변 데이터를 순회합니다.
   for (const extract of data.extracts) {
     const questionNode = figma.createText();
     questionNode.characters = extract.question_content;
@@ -65,19 +67,24 @@ async function createTextNodesFromData(data: QuestionnaireData) {
 
     const answerNodes = [];
     let totalAnswerHeight = 0;
+
+    // 각 답변에 대한 처리를 시작합니다.
     for (const answer of extract.answers) {
       const answerNode = figma.createText();
       answerNode.characters = answer;
 
-      // 그림자 효과를 answerNode 에 추가
+      // 답변의 글자 색을 검정색으로 설정합니다.
+      answerNode.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
+
+      // 답변의 음영을 약하게 설정합니다.
       answerNode.effects = [{
         type: "DROP_SHADOW",
-        color: { r: 0, g: 0, b: 0, a: 0.5 },
+        color: { r: 0, g: 0, b: 0, a: 0.3 }, // 투명도를 0.3 으로 설정
         offset: { x: 2, y: 2 },
         radius: 2,
         spread: 0,
-        blendMode: "NORMAL",  // 이 부분을 추가해주세요
-        visible: true
+        visible: true,
+        blendMode: "NORMAL"
       }];
 
       answerNode.y = yOffset;
@@ -89,11 +96,14 @@ async function createTextNodesFromData(data: QuestionnaireData) {
       answerNodes.push(answerNode);
     }
 
+    // 질문과 답변을 감싸는 박스를 생성하고 패딩을 추가합니다.
+    const padding = 10; // 패딩 값
     const wrappingBox = figma.createRectangle();
-    wrappingBox.resize(questionBox.width, questionBox.height + totalAnswerHeight);
+    wrappingBox.resize(questionBox.width + 2 * padding, questionBox.height + totalAnswerHeight + 2 * padding);
     wrappingBox.fills = [{ type: 'SOLID', color: { r: 0.9, g: 0.9, b: 0.9 } }];
-    wrappingBox.y = questionBox.y;
+    wrappingBox.y = questionBox.y - padding;
     figma.currentPage.appendChild(wrappingBox);
+
     figma.currentPage.appendChild(questionBox);
     figma.currentPage.appendChild(questionNode);
     for (const node of answerNodes) {
