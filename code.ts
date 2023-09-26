81115019b047462327989057900314de722380fe
drum-grammer <drumgrammer@gmail.com>
2023-09-26 12:35:12 +0900

    REFACTOR: 복잡한 로직을 분리하여 함수화함

--- code.ts
+++ code.ts
@@ -42,80 +42,97 @@ figma.ui.onmessage = (msg) => {
   }
 };
 
+
 async function createTextNodesFromData(data: QuestionnaireData) {
-  // 필요한 폰트를 로드
-  await figma.loadFontAsync({ family: "Inter", style: "Regular" }); // for figma
-  await figma.loadFontAsync({ family: "Inter", style: "Medium" });  // for figjam
+  await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+  await figma.loadFontAsync({ family: "Inter", style: "Medium" });
 
+  const PADDING = 20;
   let yOffset = 0;
+  let maxBoxWidth = getMaxBoxWidth(data);
 
-  // 각 질문과 답변 데이터 처리 시작
   for (const extract of data.extracts) {
-    const questionNode = figma.createText();
-    questionNode.characters = extract.question_content;
+    const { questionBox, questionNode } = createQuestion(extract, yOffset);
+    const answerNodes = createAnswers(extract, yOffset + questionBox.height + 10);
 
-    const questionBox = figma.createRectangle();
-    questionBox.resize(questionNode.width + 20, questionNode.height + 10);
-    questionBox.fills = [{ type: 'SOLID', color: { r: 0.2, g: 0.6, b: 0.8 } }];
-    questionBox.y = yOffset;
-    figma.currentPage.appendChild(questionBox);
-
-    questionNode.y = yOffset + 5;
-    figma.currentPage.appendChild(questionNode);
-
-    yOffset += questionBox.height + 10;
-
-    const answerNodes = [];
-    let totalAnswerHeight = 0;
-
-    // 각 답변에 대한 처리
-    for (const answer of extract.answers) {
-      const answerNode = figma.createText();
-      answerNode.characters = answer;
-
-      // 답변의 글자 색 설정 (검정색)
-      answerNode.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
-
-      // 답변의 음영 설정
-      answerNode.effects = [{
-        type: "DROP_SHADOW",
-        color: { r: 0, g: 0, b: 0, a: 0.3 },  // 투명도를 0.3 으로 설정
-        offset: { x: 2, y: 2 },
-        radius: 2,
-        spread: 0,
-        visible: true,
-        blendMode: "NORMAL"
-      }];
-
-      answerNode.y = yOffset;
-      figma.currentPage.appendChild(answerNode);
-
-      yOffset += answerNode.height + 5;
-      totalAnswerHeight += answerNode.height + 5;
-
-      answerNodes.push(answerNode);
-    }
-
-    // 패딩 값을 설정
-    const padding = 15; // 패딩 값
-    const wrappingBox = figma.createRectangle();
-    wrappingBox.resize(questionBox.width + 2 * padding, questionBox.height + totalAnswerHeight + 2 * padding);
-    wrappingBox.fills = [{ type: 'SOLID', color: { r: 0.9, g: 0.9, b: 0.9 } }];
-    wrappingBox.y = questionBox.y - padding;
-    wrappingBox.x = padding;  // 왼쪽 패딩을 추가합니다.
+    const wrappingBox = createWrappingBox(questionBox, answerNodes, maxBoxWidth, PADDING);
     figma.currentPage.appendChild(wrappingBox);
-
-    figma.currentPage.appendChild(questionBox);
-    figma.currentPage.appendChild(questionNode);
-    for (const node of answerNodes) {
-      figma.currentPage.appendChild(node);
-    }
 
     const group = figma.group([wrappingBox, questionBox, questionNode, ...answerNodes], figma.currentPage);
     group.name = extract.question_content;
 
-    yOffset += 20;
+    yOffset += wrappingBox.height + PADDING;
   }
 
   figma.closePlugin();
+}
+
+function getMaxBoxWidth(data: QuestionnaireData): number {
+  let maxWidth = 0;
+  for (const extract of data.extracts) {
+    const textNode = figma.createText();
+    textNode.characters = extract.question_content;
+    if (textNode.width > maxWidth) {
+      maxWidth = textNode.width;
+    }
+    for (const answer of extract.answers) {
+      textNode.characters = answer;
+      if (textNode.width > maxWidth) {
+        maxWidth = textNode.width;
+      }
+    }
+  }
+  return maxWidth;
+}
+
+function createQuestion(extract: AnswerData, yOffset: number) {
+  const questionNode = figma.createText();
+  questionNode.characters = extract.question_content;
+
+  const questionBox = figma.createRectangle();
+  questionBox.resize(questionNode.width + 20, questionNode.height + 10);
+  questionBox.fills = [{ type: 'SOLID', color: { r: 0.2, g: 0.6, b: 0.8 } }];
+  questionBox.y = yOffset;
+
+  questionNode.y = yOffset + 5;
+
+  figma.currentPage.appendChild(questionBox);
+  figma.currentPage.appendChild(questionNode);
+
+  return { questionBox, questionNode };
+}
+
+function createAnswers(extract: AnswerData, yOffset: number) {
+  const answerNodes = [];
+  for (const answer of extract.answers) {
+    const answerNode = figma.createText();
+    answerNode.characters = answer;
+    answerNode.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
+    answerNode.effects = [{
+      type: "DROP_SHADOW",
+      color: { r: 0, g: 0, b: 0, a: 0.3 },
+      offset: { x: 2, y: 2 },
+      radius: 2,
+      spread: 0,
+      visible: true,
+      blendMode: "NORMAL"
+    }];
+    answerNode.y = yOffset;
+
+    figma.currentPage.appendChild(answerNode);
+    answerNodes.push(answerNode);
+
+    yOffset += answerNode.height + 5;
+  }
+  return answerNodes;
+}
+
+function createWrappingBox(questionBox: RectangleNode, answerNodes: TextNode[], maxBoxWidth: number, padding: number) {
+  const totalHeight = questionBox.height + answerNodes.reduce((acc, node) => acc + node.height + 5, 0);
+  const wrappingBox = figma.createRectangle();
+  wrappingBox.resize(maxBoxWidth + 2 * padding, totalHeight + 2 * padding);
+  wrappingBox.fills = [{ type: 'SOLID', color: { r: 0.9, g: 0.9, b: 0.9 } }];
+  wrappingBox.y = questionBox.y - padding;
+  wrappingBox.x = (figma.viewport.bounds.width - (maxBoxWidth + 2 * padding)) / 2;
+  return wrappingBox;
 }
